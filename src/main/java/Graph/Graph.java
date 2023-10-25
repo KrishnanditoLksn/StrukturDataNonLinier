174242d8891f28354db35e8263b4c8df03e5517c
Gorthdzx <krislaksana123@gmail.com>
2023-10-25 07:21:19 +0700

    added exception to handle out of bounds in array of vertex

--- src/main/java/Graph/Graph.java
+++ src/main/java/Graph/Graph.java
@@ -1,26 +1,24 @@
 package Graph;
 
 public class Graph {
+    Vertex[] vertexList;//membuat variabel bertipe array of Vertex
+    int[][] adjacencyMatrix;//membuat variabel bertipe array 2D of vertex
+    int count;//membuat variable count
+    int amountVertex;//membuat variabel jumlah vertex
 
-    Vertex[] vertexList;
-    int[][] adjacencyMatrix;
-    int count;
-    int amountVertex;
-
-    public Graph(int amountVertex) {
-        this.vertexList = new Vertex[amountVertex];
-        this.count = 0;
-        this.adjacencyMatrix = new int[amountVertex][amountVertex];
-        this.amountVertex = amountVertex;
+    public Graph(int amountVertex) {//membuat konstruktor graph
+        this.vertexList = new Vertex[amountVertex];//membuat array of objek vertex sebanyak jumlah vertex
+        this.count = 0;//membuat variable count  untuk menghitung seberapa banyak index
+        this.adjacencyMatrix = new int[amountVertex][amountVertex];//membuat array 2d dengan jumlah banyak element sesuai param
+        this.amountVertex = amountVertex;//menunjuk variabel ke konstruktor
     }
 
 
     public void addVertex(String vertex) {
         if (count < amountVertex) {
             vertexList[count++] = new Vertex(vertex);
-
         } else {
-            System.out.println("This vertex is full" + amountVertex);
+            throw new ArrayIndexOutOfBoundsException("Out of bound of array");
         }
     }
 
@@ -29,7 +27,7 @@ public class Graph {
             adjacencyMatrix[startIndex][finishIndex] = 1;
             adjacencyMatrix[finishIndex][startIndex] = 1;
         } else {
-            System.out.println("Invalid vertex index");
+            throw new ArrayIndexOutOfBoundsException("Out of bounds of array ");
         }
     }
 
